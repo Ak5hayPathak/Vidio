2340dbcd693b7554dff96457148524db33f37e58
Akshay Pathak <akshaypathak0821@gmail.com>
2026-09-16 13:17:58 +0530

    feat: add loading state and fetch current user in AuthContext

--- client/src/components/context/AuthContext.jsx
+++ client/src/components/context/AuthContext.jsx
@@ -1,12 +1,30 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import api from "../../services/api.js";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      try {
+        const response = await api.get("/users/current-user");
+
+        setUser(response.data.data);
+      } catch (error) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getCurrentUser();
+  }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, setUser, loading }}>
       {children}
     </AuthContext.Provider>
   );
